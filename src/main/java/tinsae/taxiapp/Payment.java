b21d520943970638d464d7da352fb739c8564987
tinsae-ghilay <tinsaekahsay@gmail.com>
2024-11-30 09:53:29 +0100

    exam project set up

---
+++ src/main/java/tinsae/taxiapp/Payment.java
@@ -0,0 +1,6 @@
+package tinsae.taxiapp;
+
+interface Payment {
+
+    void pay();
+}
---
+++ src/main/java/tinsae/taxiapp/Transport.java
@@ -0,0 +1,11 @@
+package tinsae.taxiapp;
+
+/**
+ * @author Kahsay Tinsae Ghilay
+ * Transport base class
+ */
+
+public interface Transport {
+
+    void transport();
+}
