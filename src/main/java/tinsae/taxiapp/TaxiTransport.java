61451a91f19d9b58a466d32baa368bd6d36b77f8
tinsae-ghilay <tinsaekahsay@gmail.com>
2024-11-30 11:27:22 +0100

    progress

---
+++ src/main/java/tinsae/taxiapp/TaxiTransport.java
@@ -0,0 +1,17 @@
+package tinsae.taxiapp;
+
+/**
+ * Taxi ride
+ * @author kahsay Tinsae Ghilay
+ */
+public class TaxiTransport   extends   Transport{
+
+    /**
+     * transport method
+     */
+    @Override
+    public void transport() {
+        System.out.println("Transporting by taxi going on");
+    }
+}
+
