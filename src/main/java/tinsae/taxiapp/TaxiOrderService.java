40948e56abcaeabd9de3bded0584186b56e0d4ff
tinsae-ghilay <tinsaekahsay@gmail.com>
2024-11-30 10:14:52 +0100

    progress

--- src/main/java/tinsae/taxiapp/TaxiOrderService.java
+++ src/main/java/tinsae/taxiapp/TaxiOrderService.java
@@ -1,4 +1,18 @@
 package tinsae.taxiapp;
 
 public class TaxiOrderService {
+
+    /**
+     * Factory method to order a certain type of transport
+     * @param transport type of transport ordered
+     * @return Transport
+     * @see Transport
+     */
+    public Transport getTransport(String transport) {
+        return switch (transport.toLowerCase()) {
+            case "boat" -> new BoatTransport();
+            case "motor bike" -> new MotorBikeTransport();
+            default -> null;
+        };
+    }
 }
